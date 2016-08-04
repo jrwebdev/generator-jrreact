c32b6d36d01ecbd05ead46e5aa774f6961f58097
James Ravenscroft <web@jravenscroft.co.uk>
2016-08-04 14:30:41 +1200

    Use path.join for path concatenation

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -3,12 +3,13 @@
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 let config = {
-    context: __dirname + '/src',
+    context: path.join(__dirname, 'src'),
     entry: './app.js',
     output: {
-        path: __dirname + '/src',
+        path: path.join(__dirname, 'src'),
         filename: 'app-bundle.js'
     },
     devtool: 'eval-source-map',
@@ -49,7 +50,7 @@ let config = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-    config.output.path = __dirname + '/dist';
+    config.output.path = path.join(__dirname, 'dist');
     config.devtool = 'source-map';
     config.devServer = null;
     config.plugins.push(new webpack.DefinePlugin({
